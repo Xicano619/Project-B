f4c188a93642b069e20809e861924a97f9e0efef
Xicano619 <ivan.sillasnavarro@gmail.com>
2020-11-13 13:28:59 -0800

    updated the Href in index, city type; refactored syntax in the JS

--- Assets/javascript.js
+++ Assets/javascript.js
@@ -1,64 +1,69 @@
-    function returnBreweriesInfo(searchCity) {
+function returnBreweriesInfo(searchCity) {
+  var queryURL =
+    "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?categories=breweries&location=" +
+    searchCity +
+    "&limit=10&sortby=distance";
+
+  $.ajax({
+    url: queryURL,
+    headers: {
+      Authorization:
+        "Bearer EwKlpX7pyuR5IUSe_oV1U-UW6jjAkir-ffvzqKOSrC4IaKVE5pUr6Wc2Ssp_UD5sjzdkL34h0t9hzvyzYTSmMcksIfHo_NXNLo-qttFlBn9X-9eTbCy6jrtlxDamX3Yx",
+    },
+    method: "GET",
+  }).then(function (response) {
+    //Store the Region Center Coordinates
+    var centerLat = response.region.center.latitude;
+    var centerLong = response.region.center.longitude;
+
     
-    var queryURL = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?categories=breweries&location=" + searchCity +"&limit=10&sortby=distance"; 
+    //Display in console log the region center latitude and longitude
+    console.log(response.region.center.latitude);
+    console.log(response.region.center.longitude);
 
-    $.ajax({
-      url: queryURL,
-      headers: {
-        'Authorization' : 'Bearer EwKlpX7pyuR5IUSe_oV1U-UW6jjAkir-ffvzqKOSrC4IaKVE5pUr6Wc2Ssp_UD5sjzdkL34h0t9hzvyzYTSmMcksIfHo_NXNLo-qttFlBn9X-9eTbCy6jrtlxDamX3Yx'
-      },
-      method: "GET",
-    }).then(function(response) {
-      //Store the Region Center Coordinates
-      var centerLat = response.region.center.latitude;
-      var centerLong = response.region.center.longitude;
-      
-      //Display in console log the region center lattitude and longitude
-      console.log(response.region.center.latitude);
-      console.log(response.region.center.longitude);
+    //Creating variables to store the brewery/business information
+    var brewName = [];
+    var brewLat = [];
+    var brewLon = [];
+    var brewAddress1 = [];
+    var brewAddress2 = [];
+    var brewPhone = [];
+    var brewReviews = [];
+    var brewClosed = [];
+    for (let i = 0; i < response.businesses.length; i++) {
+      brewName[i] = response.businesses[i].name;
+      brewLat[i] = response.businesses[i].coordinates.latitude;
+      brewLon[i] = response.businesses[i].coordinates.longitude;
+      brewAddress1[i] = response.businesses[i].location.display_address[0];
+      brewAddress2[i] = response.businesses[i].location.display_address[1];
+      brewPhone[i] = response.businesses[i].display_phone;
+      brewReviews[i] = response.businesses[i].url;
+      brewClosed[i] = response.businesses[i].is_closed;
+      localStorage.setItem("name" + i, brewName[i]);
+      localStorage.setItem("lat" + i, brewLat[i]);
+      localStorage.setItem("lon" + i, brewLon[i]);
+      localStorage.setItem("address1" + i, brewAddress1[i]);
+      localStorage.setItem("address2" + i, brewAddress2[i]);
+      localStorage.setItem("phone" + i, brewPhone[i]);
+      localStorage.setItem("reviews" + i, brewReviews[i]);
+      localStorage.setItem("closed" + i, brewClosed[i]);
+    }
+  });
+  function breweryInfo(brewery) {
+    var infoName = "Name: " + localStorage.getItem("name" + brewery);
+    var infoAdd1 = "Address: " + localStorage.getItem("address1" + brewery);
+    var infoAdd2 = "         " + localStorage.getItem("address2" + brewery);
+    var infoPhone = "Phone: " + localStorage.getItem("phone" + brewery);
+    var infoReviews =
+      "Yelp Review: " + localStorage.getItem("reviews" + brewery);
 
-      //Creating variables to store the brewery/business information
-      var brewName = [];
-      var brewLat = [];
-      var brewLon = [];
-      var brewAddress1 = [];
-      var brewAddress2 = [];
-      var brewPhone = [];
-      var brewReviews = [];
-      var brewClosed = [];
-      for (let i = 0; i< response.businesses.length; i++){
-        brewName[i] = response.businesses[i].name;
-        brewLat[i] = response.businesses[i].coordinates.latitude;
-        brewLon[i] = response.businesses[i].coordinates.longitude;
-        brewAddress1[i] = response.businesses[i].location.display_address[0];
-        brewAddress2[i] = response.businesses[i].location.display_address[1];
-        brewPhone[i] = response.businesses[i].display_phone;
-        brewReviews[i] = response.businesses[i].url;
-        brewClosed[i] = response.businesses[i].is_closed;
-        localStorage.setItem("name" + i, brewName[i]);
-        localStorage.setItem("lat" + i, brewLat[i]);
-        localStorage.setItem("lon" + i, brewLon[i]);
-        localStorage.setItem("address1" + i, brewAddress1[i]);
-        localStorage.setItem("address2" + i, brewAddress2[i]);
-        localStorage.setItem("phone" + i, brewPhone[i]);
-        localStorage.setItem("reviews" + i, brewReviews[i]);
-        localStorage.setItem("closed" + i, brewClosed[i]);
-      };
-
-    function breweryInfo(brewery) {
-        var infoName = "Name: " + localStorage.getItem("name" + brewery);
-        var infoAdd1 = "Address: " + localStorage.getItem("address1" + brewery);
-        var infoAdd2 = "         " + localStorage.getItem("address2" + brewery);
-        var infoPhone = "Phone: " + localStorage.getItem("phone" + brewery);
-        var infoReviews = "Yelp Review: " + localStorage.getItem("reviews" + brewery);
-      
-        document.getElementById("location-info").innerHTML = infoName;
-        document.getElementById("location-info").innerHTML = infoAdd1;
-        document.getElementById("location-info").innerHTML = infoAdd2;
-        document.getElementById("location-info").innerHTML = infoPhone;
-        document.getElementById("location-info").innerHTML = infoReviews;  
-      }
-
-$(document).ready(function() {
-  $('.sidenav').sidenav();
+    document.getElementById("location-info").innerHTML = infoName;
+    document.getElementById("location-info").innerHTML = infoAdd1;
+    document.getElementById("location-info").innerHTML = infoAdd2;
+    document.getElementById("location-info").innerHTML = infoPhone;
+    document.getElementById("location-info").innerHTML = infoReviews;
+  }
+}
+$(document).ready(function () {
+  $(".sidenav").sidenav();
 });
