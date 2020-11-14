b513075677c078573f4bc8e10e48f4c4d4c365ea
Xicano619 <ivan.sillasnavarro@gmail.com>
2020-11-13 16:46:05 -0800

    Lat and Lon for cities in theMap.js

--- Assets/theMap.js
+++ Assets/theMap.js
@@ -15,3 +15,33 @@ function initMap() {
       });
   }
 }
+document.getElementById("link1").addEventListener("click", function() {
+    map = new google.maps.Map(document.getElementById("map"), {
+        center: { lat: 33.20303085967621, lng: -117.33123779296875 }, // default to start at current location  (user can set address if they want to change this)
+        zoom: 12
+    })
+});
+document.getElementById("link2").addEventListener("click", function() {
+    map = new google.maps.Map(document.getElementById("map"), {
+        center: { lat: 33.18291965337924, lng: -117.24128723144531 }, // default to start at current location  (user can set address if they want to change this)
+        zoom: 12
+    })
+});
+document.getElementById("link3").addEventListener("click", function() {
+    map = new google.maps.Map(document.getElementById("map"), {
+        center: { lat: 32.790569394537286, lng: -117.15408325195312 }, // default to start at current location  (user can set address if they want to change this)
+        zoom: 12
+    })
+});
+document.getElementById("link4").addEventListener("click", function() {
+    map = new google.maps.Map(document.getElementById("map"), {
+        center: { lat: 32.919645008613585, lng: -117.14515686035156 }, // default to start at current location  (user can set address if they want to change this)
+        zoom: 12
+    })
+});
+document.getElementById("link5").addEventListener("click", function() {
+    map = new google.maps.Map(document.getElementById("map"), {
+        center: { lat: 32.6451147551742, lng: -117.01332092285156 }, // default to start at current location  (user can set address if they want to change this)
+        zoom: 12
+    })
+});
